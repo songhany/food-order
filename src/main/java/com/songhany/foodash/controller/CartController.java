90a9c92d47a81c8274317d3063630e90fb23d64b
Songhan Yu <songhan@songhans-mbp.myfiosgateway.com>
2022-04-10 18:53:05 -0400

    update latest

---
+++ src/main/java/com/songhany/foodash/controller/CartController.java
@@ -0,0 +1,18 @@
+package com.songhany.foodash.controller;
+
+import com.songhany.foodash.entity.Cart;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class CartController {
+
+    @RequestMapping(value = "/cart", method = RequestMethod.GET)
+    @ResponseBody
+    public Cart getCart(){
+        return new Cart();
+    }
+}
+
